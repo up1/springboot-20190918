cf92b3ad234b092f12ed3430c20a7a6a8fd7da08
up1 <somkiat.p@gmail.com>
2019-09-18 14:01:05 +0700

    Custom metric

--- src/main/java/com/example/somkiat/HelloController.java
+++ src/main/java/com/example/somkiat/HelloController.java
@@ -1,5 +1,7 @@
 package com.example.somkiat;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,15 +23,27 @@ public class HelloController {
 
     private static final Logger log = LoggerFactory.getLogger(HelloController.class);
 
+    @Autowired
+    private MeterRegistry meterRegistry;
 
     @GetMapping("/call")
     public String call() {
         log.info("Start call");
-        String result =
-                restTemplate.getForObject(
-                        "http://10.10.99.128:8080/call", String.class);
-        log.info("Finish call");
-        return result;
+        try {
+            String result =
+                    restTemplate.getForObject(
+                            "http://10.10.99.128:8080/call", String.class);
+            log.info("Finish call");
+            meterRegistry.counter("call_xxx",
+                    "status", "success",
+                    "exception", "N/A").increment();
+            return result;
+        } catch(Exception e) {
+            meterRegistry.counter("call_xxx",
+                    "status", "fail"
+            , "exception", e.getMessage()).increment();
+        }
+        return "N/A";
     }
 
 }
