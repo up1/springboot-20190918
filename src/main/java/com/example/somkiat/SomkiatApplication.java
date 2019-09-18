fbd27e5aee0da0c50e5cfb6bbfaabd5f054d114e
up1 <somkiat.p@gmail.com>
2019-09-18 10:09:44 +0700

    Start workshop

---
+++ src/main/java/com/example/somkiat/SomkiatApplication.java
@@ -0,0 +1,13 @@
+package com.example.somkiat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomkiatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomkiatApplication.class, args);
+	}
+
+}
